36b06427a706a744a45f4f7b1962a9e937ceb63d
Tom Streller <tom.streller@functional-games.de>
2019-09-09 14:53:00 +0200

    Add testing

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -5,27 +5,28 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use an_os::println;
 
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-  println!("Hello World{}", "!");
-
-  #[cfg(test)]
   test_main();
 
   loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-  println!("{}", info);
-  loop {}
+fn test_runner(tests: &[&dyn Fn()]) {
+  unimplemented!();
 }
 
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
   an_os::test_panic_handler(info)
+}
+
+use an_os::{println, serial_print, serial_println};
+
+#[test_case]
+fn test_println() {
+  serial_print!("test_println... ");
+  println!("test_println output");
+  serial_println!("[ok]");
 }
